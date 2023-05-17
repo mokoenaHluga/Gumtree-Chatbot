5378007638953b68df1efc71d1570704d20a0d80
Lehlohonolo Mokoena <mokoena.hluga@gmail.com>
2023-05-17 16:19:50 +0200

    Initial commit

---
+++ src/main/java/com/cos730/chatbot/gumtree/GumtreeChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.cos730.chatbot.gumtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GumtreeChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GumtreeChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cos730/chatbot/gumtree/GumtreeChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos730.chatbot.gumtree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GumtreeChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
